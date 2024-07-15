311a0504a9a44d962c3ca7ef47c80242ad96c88b
Alessandro Assis <aledev21@gmail.com>
2024-07-15 11:04:41 -0300

    Windows compatibility removed

--- src/include/memory_manager.h
+++ src/include/memory_manager.h
@@ -1,31 +1,22 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 
-#ifdef _WIN32
-#include <Windows.h>
-#else
 #include <unistd.h>
 #include <sys/types.h>
-#endif
+
 
 typedef struct MemoryManager {
-#ifdef _WIN32
-    HANDLE hProcess;
-    DWORD pid;
-#else
+    
     pid_t pid;
-#endif
+
 } MemoryManager;
 
-MemoryManager* MemoryManager_new(DWORD pid);
+MemoryManager* MemoryManager_new(pid_t pid);
+
 void MemoryManager_delete(MemoryManager* mm);
 
-#ifdef _WIN32
 int MemoryManager_openProcess(MemoryManager* mm);
-int MemoryManager_writeInt(MemoryManager* mm, LPVOID address, int value);
-#else
-int MemoryManager_openProcess(MemoryManager* mm);
-int MemoryManager_writeInt(MemoryManager* mm, void* address, int value);
-#endif
 
-#endif // MEMORY_MANAGER_H
+int MemoryManager_writeInt(MemoryManager* mm, void* address, int value);
+
+#endif 
