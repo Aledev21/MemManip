fd89aed6109d87fb4e943ea3e766946b15e40b6d
Alessandro Assis <aledev21@gmail.com>
2024-07-15 14:54:01 -0300

    Calls the callback function with the PID and user data. and close dir

--- src/list_process.c
+++ src/list_process.c
@@ -4,33 +4,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-void list_processes(ProcessSelectedCallback callback, gpointer user_data) 
-{
+void list_processes(ProcessSelectedCallback callback, gpointer user_data) {
     DIR *dir;
     struct dirent *entry;
 
     dir = opendir("/proc");
-    if (dir == NULL) 
-    {
+    if (dir == NULL) {
         perror("opendir");
         return;
     }
 
-    while ((entry = readdir(dir)) != NULL) 
-    {
-        if (entr ->d_type == DT_DIR && atoi(entry->d_name) != 0)
-        {
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR && atoi(entry->d_name) != 0) {
             pid_t pid = atoi(entry->d_name);
             char filename[512];
             snprintf(filename, sizeof(filename), "/proc/%d/cmdline", pid);
-        }
 
-        FILE *fp = fopen(filename, "r");
-        if (fp != NULL) 
-        {
-            char cmdline[256];
-
-            
+            FILE *fp = fopen(filename, "r");
+            if (fp != NULL) {
+                char cmdline[256];
+                if (fgets(cmdline, sizeof(cmdline), fp)) {
+                    cmdline[strcspn(cmdline, "\n")] = '\0';
+                    callback(pid, user_data);
+                }
+                fclose(fp); 
+            }
         }
     }
+
+    closedir(dir);
 }
