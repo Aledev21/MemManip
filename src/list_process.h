aad63d776fa39ce08fad0b949a8e9960987c352e
Alessandro Assis <aledev21@gmail.com>
2024-07-15 16:55:03 -0300

    fix listprocess

--- src/list_process.h
+++ src/list_process.h
@@ -2,10 +2,9 @@
 #define LIST_PROCESS_H
 
 #include <gtk/gtk.h>
-#include "memory_manager.h"
 
 typedef void (*ProcessSelectedCallback)(pid_t pid, gpointer user_data);
 
 void list_processes(ProcessSelectedCallback callback, gpointer user_data);
 
-#endif 
+#endif
